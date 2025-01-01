f9f849a10b6a02fce22c44e812164a6955739ca4
Daniel Bui <93468548+imdanielbui@users.noreply.github.com>
2025-01-01 22:59:18 +0700

    fix fb

--- src/pages/Share/Share.tsx
+++ src/pages/Share/Share.tsx
@@ -1,4 +1,5 @@
 import { Button, Stack, Typography } from "@mui/material";
+import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
 
 const Share = () => {
@@ -9,14 +10,26 @@ const Share = () => {
   const goToShareWithEmailPage = () => {
     navigate("/share-with-email");
   };
+  const handleShare = () => {
+    const url = "https://listening-to-customer.vercel.app/share-with-email";
+    const fbShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    window.open(fbShareUrl, "_blank", "width=600,height=400");
+  };
   return (
     <Stack bgcolor={"#051e40"} alignItems={"center"} height={"100vh"} padding={2} justifyContent={"center"}>
+      <Helmet>
+        <meta property="og:title" content="Trang chia sẻ lên Facebook" />
+        <meta property="og:description" content="Một trang nội dung hay để chia sẻ." />
+        <meta property="og:image" content="../../assets/thumbnails/tn-lever5.jpg" />
+        <meta property="og:url" content="https://listening-to-customer.vercel.app/share-with-email" />
+        <meta property="og:type" content="website" />
+      </Helmet>
       <Stack boxShadow={1} bgcolor={"white"} borderRadius={2} minWidth={355} maxWidth={700} justifyContent={"center"} alignItems={"center"} padding={3} gap={2}>
         <Typography variant="h6" color={"#051e40"}>
           Chia sẻ
         </Typography>
         <Typography>Đây là một số cách bạn có thể chia sẻ với bạn bè và đồng nghiệp của mình:</Typography>
-        <Button fullWidth variant="contained">
+        <Button fullWidth variant="contained" onClick={handleShare}>
           Chia sẻ qua Facebook
         </Button>
         <Button fullWidth variant="contained" sx={{ bgcolor: "#e9f4ff", color: "#2592f2", boxShadow: 0 }} onClick={goToShareWithEmailPage}>
